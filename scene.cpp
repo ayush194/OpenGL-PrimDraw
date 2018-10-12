d63a61e5bed05daa7d1b7f5109fc91ee9178cc96
Ayush Kumar <kr.ayush998@gmail.com>
2018-10-12 21:13:09 +0530

    Modified scene.cpp

--- scene.cpp
+++ scene.cpp
@@ -32,7 +32,7 @@ float DELTATIME = 0.0;
 float CURSOR_POS_X = SCR_WIDTH / 2;
 float CURSOR_POS_Y = SCR_HEIGHT / 2;
 bool FIRST_MOUSE = true;
-Camera CAMERA(glm::vec3(0.0, 0.0, 5.0));
+Camera CAMERA(glm::vec3(0.0, 1.0, 1.0));
 
 void mouse_callback(GLFWwindow*, double, double);
 void scroll_callback(GLFWwindow*, double, double);
@@ -50,7 +50,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
     
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpengl", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Opengl-Dryrun", NULL, NULL);
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
@@ -69,21 +69,41 @@ int main() {
     Scene myscene;
     //myscene.setSmooth(true);
     //myscene.createObject(ELLIPSOID, {1.0, 1.0, 1.0});
-    int cube = myscene.createObject(CUBOID, {0.0, 0.0, 5.0});
-    myscene.setUvmap(cube, FACE);
-    int t1 = myscene.createTexture("resources/textures/crate_diffuse.png");
-    int t2 = myscene.createTexture("resources/textures/crate_specular.png");
-    int t3 = myscene.createTexture("resources/textures/crate_emission.jpg");
-    myscene.setTexture(DIFFUSE, cube, t1);
-    myscene.setTexture(SPECULAR, cube, t2);
-    myscene.setTexture(EMISSION, cube, t3);
-    uint plane = myscene.createObject(RECTANGULAR_PLANE, {0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}, {10.0, 10.0, 10.0}, 0, true, 1, 0);
+    //myscene.setSmooth(true);
+    //myscene.setSubd(10, 10);
+    int sphere = myscene.createObject(ELLIPSOID, {0.0, 0.0, 0.0});
+    //int cube = myscene.createObject(CUBOID, {0.0, 0.0, 5.0});
+    //myscene.setUvmap(cube, FACE);
+    //int t1 = myscene.createTexture("resources/textures/crate_diffuse.png");
+    //int t2 = myscene.createTexture("resources/textures/crate_specular.png");
+    //int t3 = myscene.createTexture("resources/textures/crate_emission.jpg");
+    //myscene.setTexture(DIFFUSE, cube, t1);
+    //myscene.setTexture(SPECULAR, cube, t2);
+    //myscene.setTexture(EMISSION, cube, t3);
+    //uint plane = myscene.createObject(RECTANGULAR_PLANE, {0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}, {10.0, 10.0, 10.0}, 0, true, 1, 0);
+    int gold = myscene.createMaterial(EMERALD);
+    myscene.setMaterial(sphere, gold);
     myscene.setLightAmbient({0.1f, 0.1f, 0.1f});
     myscene.setLightDiffuse({0.4f, 0.4f, 0.4f});
     myscene.setLightSpecular({0.4f, 0.4f, 0.4f});
-    myscene.setLightConstants({1.0, 0.09, 0.032});
+    myscene.setLightConstants({1.0, 0.4, 0.032});
     //myscene.createLight(DIRECTEDLIGHT, plane, {5.0, 5.0, 5.0}, {0.0, 0.0, -1.0});
-    myscene.createLight(DIRECTEDLIGHT, plane, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0});
+    myscene.createLight(DIRECTEDLIGHT, NULL, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0});
+    int x = 3;
+    myscene.createLight(POINTLIGHT, NULL, {x, x, x}, {-x, -x, -x});
+    myscene.createLight(POINTLIGHT, NULL, {x, x, -x}, {-x, -x, x});
+    myscene.createLight(POINTLIGHT, NULL, {x, -x, x}, {-x, x, -x});
+    myscene.createLight(POINTLIGHT, NULL, {-x, x, x}, {x, -x, -x});
+    myscene.createLight(POINTLIGHT, NULL, {x, -x, -x}, {-x, x, x});
+    myscene.createLight(POINTLIGHT, NULL, {-x, -x, x}, {x, x, -x});
+    myscene.createLight(POINTLIGHT, NULL, {-x, x, -x}, {x, -x, x});
+    myscene.createLight(POINTLIGHT, NULL, {-x, -x, -x}, {x, x, x});
+    //myscene.createLight(POINTLIGHT, NULL, {0.0, 0.0, 2.0}, {0.0, 0.0, -1.0});
+    //myscene.createLight(POINTLIGHT, NULL, {0.0, 0.0, -5.0}, {0.0, 0.0, 1.0});
+    //myscene.createLight(POINTLIGHT, NULL, {0.0, 5.0, 0.0}, {0.0, -1.0, 0.0});
+    //myscene.createLight(POINTLIGHT, NULL, {0.0, -5.0, 0.0}, {0.0, 1.0, 0.0});
+    //myscene.createLight(POINTLIGHT, NULL, {5.0, 0.0, 0.0}, {-1.0, 0.0, 0.0});
+    //myscene.createLight(POINTLIGHT, NULL, {-2.0, 0.0, 0.0}, {1.0, 0.0, 0.0});
 
     while (!glfwWindowShouldClose(window)) {
         //process input
